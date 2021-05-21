56440c6fc1c1ddf1207421f1d73006b4e0055473
Ricardo Larrahondo <ricardo.larrahondo@avaldigitallabs.com>
2021-05-21 14:19:18 -0500

    :fire: Remove parallel test execution due to a sync problem with stdout
    :construction_worker: Update deploy reports logic to include the nested folder

--- src/test/kotlin/io/github/ricardorlg/devicefarm/tractor/ProjectConfig.kt
+++ src/test/kotlin/io/github/ricardorlg/devicefarm/tractor/ProjectConfig.kt
@@ -4,6 +4,6 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.extensions.htmlreporter.HtmlReporter
 
 object ProjectConfig : AbstractProjectConfig() {
-    override val parallelism = 16
+    override val parallelism = 1
     override fun listeners() = listOf(HtmlReporter())
 }
